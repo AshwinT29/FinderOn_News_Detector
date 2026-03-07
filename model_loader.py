966f773cc789dde80b20bebef57148ed89444dd7
AbinRaj2004 <167908750+AbinRaj2004@users.noreply.github.com>
2026-03-07 17:37:35 +0530

    Fixed prediction logic

--- model_loader.py
+++ model_loader.py
@@ -2,8 +2,14 @@ import torch
 import torchvision.models as models
 
 def load_model():
-    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+    # DO NOT download weights
+    model = models.resnet18(weights=None)
+
+    # Change final layer
     model.fc = torch.nn.Linear(model.fc.in_features, 2)
+
+    # Load your trained weights
     model.load_state_dict(torch.load("model_weights.pth", map_location="cpu"))
+
     model.eval()
     return model
